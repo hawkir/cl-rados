e7e324e9d5eb17d32491db209350abcb82a2347d
rick <rick@porkpie.state51.co.uk>
2014-11-11 16:37:24 +0000

    cleaning up example for code reuse

--- cl-rados.lisp
+++ cl-rados.lisp
@@ -6,90 +6,57 @@
 (load-foreign-library "librados.so")
 
 
+(defmacro! with-rados ((cluster io &key id keyring conf-file pool-name) &body body)
+  (assert (and id keyring conf-file pool-name))
+  `(with-foreign-object (,g!*cluster :pointer)
+     (assert (>= (rados_create ,g!*cluster ,id) 0))
+     (let ((,cluster (mem-ref ,g!*cluster :pointer)))
+       (unwind-protect
+            (progn 
+              (assert (>= (rados_conf_set ,cluster
+                                          "keyring"
+                                          ,keyring)
+                          0))
+              (assert (>= (rados_conf_read_file ,cluster
+                                                ,conf-file)
+                          0))
+              (assert (>= (rados_connect ,cluster)))
+              (with-foreign-object (,g!*io :pointer)
+                (progn
+                  (assert (>= (rados_ioctx_create ,cluster
+                                                  ,pool-name
+                                                  ,g!*io)
+                              0))
+                  (let ((,io (mem-ref ,g!*io :pointer)))
+                    (unwind-protect
+                         (progn ,@body)
+                      (rados_ioctx_destroy ,io ))))))
+         (rados_shutdown ,cluster)))))
+       
+
+(defun dump-ceph-obj-to-file (cluster io file-id filespec)
+  (with-foreign-object (*res :uchar 1000)
+    (with-open-file (dumpfile filespec :direction :output
+                              :if-exists :supersede
+                              :if-does-not-exist :create
+                              :element-type 'octet)
+      (let ((num-bytes (print (rados_read io
+                                          file-id *res 1000 0))))
+        (assert (>= num-bytes 0))
+        (print (loop for i below num-bytes
+                  do (write-byte (mem-aref *res :uchar i) dumpfile)))))))
+
+(defun write-string-to-ceph (cluster io file-id string)
+  (assert (>= (rados_write_full io
+                                file-id string (length (string-to-octets string)))
+              0)))
+
 ;; int main (int argc, char* argv []) {
 (defun main ()
-  ;; int err;
-  ;; rados_t cluster;
-  (with-foreign-object (*cluster :pointer)
-    ;;   err = rados_create(&cluster, "platform");
-    ;;   if (err < 0) {
-    ;;     fprintf(stderr, "%s: cannot create a cluster handle: %s\n", argv[0], strerror(-err));
-    ;;     exit(1);
-    ;;   }
-    (assert (>= (rados_create *cluster "platform") 0))
-
-    ;;   err = rados_conf_set(cluster, "keyring", "/etc/ceph/ceph.client.admin.keyring");
-    ;;   if (err < 0) {
-    ;;     fprintf(stderr, "%s: keyring failed because: %s\n", argv[0], strerror(-err));
-    ;;     exit(1);
-    ;;   }
-    (assert (>= (rados_conf_set (mem-ref *cluster :pointer)
-                                "keyring"
-                                "/etc/ceph/ceph.client.admin.keyring")
-                0))
-    
-    ;;   err = rados_conf_read_file(cluster, "/home/rick/Downloads/ceph.conf");
-    ;;   if (err < 0) {
-    ;;     fprintf(stderr, "%s: cannot read config file: %s\n", argv[0], strerror(-err));
-    ;;     exit(1);
-    ;;   }
-    (assert (>= (rados_conf_read_file (mem-ref *cluster :pointer)
-                                      "/home/rick/Downloads/ceph.conf")
-                0))
-    ;;   err = rados_connect(cluster);
-    ;;   if (err < 0) {
-    ;;     fprintf(stderr, "%s: cannot connect to cluster: %s\n", argv[0], strerror(-err));
-    ;;     exit(1);
-    ;;   }
-    ;;   rados_ioctx_t io;
-    ;;   char *poolname = "platform";
-    (assert (>= (rados_connect (mem-ref *cluster :pointer))))
-    
-
-    ;;   err = rados_ioctx_create(cluster, poolname, &io);
-    ;;   if (err < 0) {
-    ;;     fprintf(stderr, "%s: cannot open rados pool %s: %s\n", argv[0], poolname, strerror(-err));
-    ;;     rados_shutdown(cluster);
-    ;;     exit(1);
-    ;;   }
-    (with-foreign-object (*io :pointer)
-      (assert (>= (rados_ioctx_create (mem-ref *cluster :pointer)
-                                      "platform"
-                                      *io)
-                  0))
-      ;;   err = rados_write_full(io, "greeting", "hello", 5);
-      ;;   if (err < 0) {
-      ;;     fprintf(stderr, "%s: cannot write pool %s: %s\n", argv[0], poolname, strerror(-err));
-      ;;     rados_ioctx_destroy(io);
-      ;;     rados_shutdown(cluster);
-      ;;     exit(1);
-      ;;   }
-      (assert (>= (rados_write_full (mem-ref *io :pointer)
-                                    "greeting" "hello" 5)
-                  0))
-      ;;   char res[1000];
-      (with-foreign-object (*res :uchar 1000)
-        (with-open-file (foo "~/foo" :direction :output
-                             :if-exists :supersede
-                             :if-does-not-exist :create
-                             :element-type 'octet)
-          ;;   err = rados_read(io, "greeting", res, 1000, 0);
-          (let ((num-bytes (print (rados_read (mem-ref *io :pointer)
-                                              "greeting" *res 1000 0))))
-            (assert (>= num-bytes 0))
-            (print (loop for i below num-bytes
-                      do (write-byte (mem-aref *res :uchar i) foo)))))
-            ;;   if (err < 0) {
-            ;;     fprintf(stderr, "%s: failed to read from pool %s: %s\n", argv[0], poolname, strerror(-err));
-            ;;     rados_ioctx_destroy(io);
-            ;;     rados_shutdown(cluster);
-            ;;     exit(1);
-            ;;   }
-            
-            ;;   printf("read back %s\n\n", res);
-            ;;   rados_ioctx_destroy(io);
-            ;;   rados_shutdown(cluster);
-            (rados_ioctx_destroy (mem-ref *io :pointer))
-            (rados_shutdown (mem-ref *cluster :pointer))))))
-;; }
-
+  (with-rados (cluster io
+                       :id "platform"
+                       :keyring "/etc/ceph/ceph.client.admin.keyring"
+                       :conf-file "/home/rick/Downloads/ceph.conf"
+                       :pool-name "platform")
+    (write-string-to-ceph cluster io "greeting" "heyheyhey")
+    (dump-ceph-obj-to-file cluster io "greeting" "~/bar")))
--- package.lisp
+++ package.lisp
@@ -1,5 +1,7 @@
 ;;;; package.lisp
 
 (defpackage #:cl-rados
-  (:use #:cl #:cffi #:flexi-streams))
+  (:use #:cl #:cffi #:flexi-streams #:trivial-gray-streams #:let-over-lambda))
 
+(defpackage #:cl-rados.graystream-example
+  (:use :cl :cl-rados #:trivial-gray-streams))
