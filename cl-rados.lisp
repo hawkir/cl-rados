7b77584da5e56de3613d8c4e6232784016acbd96
rick <rick@porkpie.state51.co.uk>
2014-11-11 19:05:18 +0000

    read/write arbitrary binary data to/from ceph

--- cl-rados.lisp
+++ cl-rados.lisp
@@ -34,29 +34,68 @@
          (rados_shutdown ,cluster)))))
        
 
-(defun dump-ceph-obj-to-file (cluster io file-id filespec)
-  (with-foreign-object (*res :uchar 1000)
-    (with-open-file (dumpfile filespec :direction :output
-                              :if-exists :supersede
-                              :if-does-not-exist :create
-                              :element-type 'octet)
-      (let ((num-bytes (print (rados_read io
-                                          file-id *res 1000 0))))
-        (assert (>= num-bytes 0))
-        (print (loop for i below num-bytes
-                  do (write-byte (mem-aref *res :uchar i) dumpfile)))))))
+(defun dump-ceph-obj-to-file (io file-id filespec &key (chunk-size 1000))
+  (with-open-file (dumpfile filespec :direction :output
+                            :if-exists :supersede
+                            :if-does-not-exist :create
+                            :element-type 'octet)
+    (with-foreign-object (*res :uchar chunk-size)
+      (let ((pos 0))
+        (loop
+           for n = (let ((num-bytes (rados_read io
+                                                file-id *res chunk-size pos)))
+                     (assert (>= num-bytes 0))
+                     (incf pos num-bytes)
+                     (loop for i below num-bytes
+                        do (write-byte (print (mem-aref *res :uchar i)) dumpfile))
+                     num-bytes)
+           until (= 0 n))
+           ;;uncomment this to get a summary of the bytes read in each pass
+           ;; collect n
+        pos))))
 
-(defun write-string-to-ceph (cluster io file-id string)
+(defun write-octets-to-ceph (io file-id octets)
+  (let ((bytes (make-array (length octets)
+                           :element-type '(unsigned-byte 8)
+                           :initial-contents octets)))
+    (with-pointer-to-vector-data (*bytes bytes)
+      (assert (>= (rados_write_full io
+                                    file-id *bytes (length bytes))
+                  0)))))
+
+(defun write-string-to-ceph (io file-id string)
   (assert (>= (rados_write_full io
                                 file-id string (length (string-to-octets string)))
               0)))
 
-;; int main (int argc, char* argv []) {
 (defun main ()
   (with-rados (cluster io
                        :id "platform"
                        :keyring "/etc/ceph/ceph.client.admin.keyring"
                        :conf-file "/home/rick/Downloads/ceph.conf"
                        :pool-name "platform")
-    (write-string-to-ceph cluster io "greeting" "heyheyhey")
-    (dump-ceph-obj-to-file cluster io "greeting" "~/bar")))
+    ;; (write-string-to-ceph io "greeting" "foobarbaz")
+    (dump-ceph-obj-to-file io "iTunes__iTunes_AU__80030548_0114_AU.txt.gz" "~/bar"  :chunk-size 3)))
+
+(defun test-read (ceph-id)
+  (with-rados (cluster io
+                       :id "platform"
+                       :keyring "/etc/ceph/ceph.client.admin.keyring"
+                       :conf-file "/home/rick/Downloads/ceph.conf"
+                       :pool-name "platform")
+    ;; (write-string-to-ceph io "greeting" "foobarbaz")
+    (dump-ceph-obj-to-file io ceph-id
+                           (concatenate 'string "/home/rick/ceph-test/" ceph-id)
+                           :chunk-size 1000)))
+
+(defun binary-garbage-loopback (ceph-id)
+  (with-rados (cluster io
+                       :id "platform"
+                       :keyring "/etc/ceph/ceph.client.admin.keyring"
+                       :conf-file "/home/rick/Downloads/ceph.conf"
+                       :pool-name "platform")
+    ;; (write-string-to-ceph io "greeting" "foobarbaz")
+    (write-octets-to-ceph io ceph-id (make-array 256 :initial-contents (loop for i from 0 to 255 collect i)))
+    (dump-ceph-obj-to-file io ceph-id
+                           (concatenate 'string "/home/rick/ceph-test/" ceph-id)
+                           :chunk-size 2)))
