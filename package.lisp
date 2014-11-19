228a7846b1331c4a83e9ca8828d635038729ddd0
rick <rick@porkpie.state51.co.uk>
2014-11-19 17:13:57 +0000

    export the relevant symbols to use the ceph api

--- package.lisp
+++ package.lisp
@@ -1,4 +1,8 @@
 ;;;; package.lisp
 
 (defpackage #:cl-rados
-  (:use #:cl #:cffi #:flexi-streams #:trivial-gray-streams #:let-over-lambda))
+  (:use #:cl #:cffi #:flexi-streams #:trivial-gray-streams #:let-over-lambda)
+  (:export :with-open-cephfile
+           :ceph-open
+           :with-rados
+           :dump-ceph-obj-to-file))
